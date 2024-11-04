41bcf1336a0a5ddd70e7785c2760171cf2ad0ce2
Rizani Husyairi <leo.rizan68@gmail.com>
2024-11-04 11:57:24 +0800

    first commit

---
+++ 03-writing-test/index.test.js
@@ -0,0 +1,17 @@
+import { test } from 'node:test';
+import assert from 'node:assert';
+import { sum } from './index.mjs';
+
+test('should add correctly', () => {
+    // Arrange
+    const operandA = 1;
+    const operandB = 1;
+  
+    // Action
+    const actualValue = sum(operandA, operandB);
+  
+    // Assert
+    const expectedValue = 2;
+    assert.equal(actualValue, expectedValue);
+  });
+
