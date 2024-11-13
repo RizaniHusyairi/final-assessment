d386356d4db0caa4dd22b3262a702f10c3194c49
Rizani Husyairi <leo.rizan68@gmail.com>
2024-11-13 15:45:48 +0800

    menyelesaikan optional 07 real world scenario

--- optional-07-real-world-scenario/orders.js
+++ optional-07-real-world-scenario/orders.js
@@ -5,18 +5,53 @@ function generateUniqueId() {
 
 
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+let orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  let price = 0;
+  for (let i = 0; i < items.length; i++){
+      price += items[i].price;
+    }
+
+  let tempOrder = {
+    id : generateUniqueId(),
+    customerName : customerName,
+    items : items,
+    totalPrice : price,
+    status: 'Menunggu'
+  }
+  orders.push(tempOrder);
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  const updateOrder = orders.map(order => {
+    if(order.id == orderId){
+      return order.status = status;
+    }else{
+      return order
+    }
+  })
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  let totalRevenue = 0;
+  orders.forEach(order => {
+    
+    if(order.status == "Selesai"){
+      totalRevenue+=order.totalPrice;
+    }
+    
+  })
+  return totalRevenue;
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  const deleteOrder = orders.filter(order => order.id !== id);
+  orders = deleteOrder
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
